7d22b113f438306742bb3164a7de95e7e791c53e
Росляков Сергей Сергеевич <roslyakov.ss2192@okeit.edu>
2025-12-15 18:38:37 +0500

    Обновлен MainActivity для работы с моделями данных

--- app/src/main/java/com/example/course_project/MainActivity.java
+++ app/src/main/java/com/example/course_project/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.course_project;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,10 +10,20 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.course_project.databinding.ActivityMainBinding;
+import com.example.course_project.models.Product;
+import com.example.course_project.models.QualityCheck;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private ArrayList<Product> productList = new ArrayList<>();
+    private ArrayList<QualityCheck> checkList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +32,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        // Настройка навигации
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_scan,
+                R.id.navigation_history,
+                R.id.navigation_analytics,
+                R.id.navigation_profile
+        ).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        initTestData();
+
+        Toast.makeText(this, "Приложение QualityCheck запущено", Toast.LENGTH_SHORT).show();
     }
 
+    private void initTestData() {
+        productList.add(new Product("1", "4601234567890", "Молоко 3.2%", "Молочные продукты", "Простоквашино"));
+        productList.add(new Product("2", "4601234567891", "Хлеб Бородинский", "Хлебобулочные изделия", "Хлебзавод №1"));
+
+        String currentDate = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(new Date());
+        checkList.add(new QualityCheck("1", "1", currentDate, 5, "Отличное качество"));
+        checkList.add(new QualityCheck("2", "2", currentDate, 4, "Хорошее качество, свежий"));
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public ArrayList<QualityCheck> getCheckList() {
+        return checkList;
+    }
 }
